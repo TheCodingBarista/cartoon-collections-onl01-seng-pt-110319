dd73bae09cf44067aa60412a2665bd9965c4d43a
Melissa Holt <melissa.holt21@gmail.com>
2019-11-15 05:36:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.detect? { array[i]}
+  if array.detect? { array[i] }
     cheese_types[i]
   else
     nil
